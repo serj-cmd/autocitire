025bcd6fd312128ff4fdb10bd57680e33ed5f3c3
serj-cmd <63778395+serj-cmd@users.noreply.github.com>
2020-05-14 16:04:35 +0300

    my project

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('home');
+});
+	
+//    return view('intraincont');
+//
+//Route::get('contnou', function () {
+//    return view('contnou');
+//});
+
+//Route::get('infoutile', function () {
+    //return view('infoutile');
+//});
+
+//Route::get('contact', function () {
+   // return view('contact');
+//});
+
